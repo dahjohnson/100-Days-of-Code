6addc88a32a6ff47340a5a16d4fdac6ceef75742
dahjohnson <djohnson@devopslabs4me.com>
2024-08-01 21:50:06 -0400

    added Day_3 file

---
+++ Day_3/treasure_island_game.py
@@ -0,0 +1,41 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction = input("Which direction will you take? Left or Right? ")
+if direction == "Left":
+    swim_or_wait = input("You have arrived at a lake. Do you wish to Swim or Wait? ")
+    if swim_or_wait == "Wait":
+        which_door = input("Three doors magically appear, which door will you walk through? Red, Blue, or Yellow? ")
+        if which_door == "Yellow":
+            print("Congrats! You've found the treasure!")
+        elif which_door == "Red":
+            print("Whoops.. You got burned by fire. Game Over!")
+        elif which_door == "Blue":
+            print("Ouch... You've been eaten by beasts. Game Over!")
+        else:
+            print("GAME OVER!!!")
+    else:
+        print("You were attacked by trout. GAME OVER!!!")
+else:
+    print("You fell into a hole. GAME OVER!!!")
